63b217718d5135cae9e5a85dd135a2afeb4ca704
wada <wadayugo@keio.jp>
2024-09-08 23:16:21 +0900

    feat: add functions for CLI

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,48 @@
+use clap::{Parser, Subcommand};
 use my_llm_client::llm_client::MyLLMClient;
+
+#[derive(Parser)]
+struct Cli {
+    #[arg(short, long)]
+    provider: Provider,
+    #[command(subcommand)]
+    cmd: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Chat,
+}
+
+#[derive(Clone)]
+enum Provider {
+    Mock,
+    ChatGPT,
+}
+
+impl From<&str> for Provider {
+    fn from(s: &str) -> Self {
+        match s {
+            "mock" => Provider::Mock,
+            "chatgpt" => Provider::ChatGPT,
+            _ => panic!("Invalid provider"),
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() {
-    let llm_client = MyLLMClient::new(false);
-    let message = llm_client.chat("こんにちは").await;
-    println!("{}", message);
+    let cli = Cli::parse();
+    match cli.provider {
+        Provider::Mock => {
+            let llm_client = MyLLMClient::new(true);
+            let message = llm_client.chat("こんにちは").await;
+            println!("{}", message);
+        }
+        Provider::ChatGPT => {
+            let llm_client = MyLLMClient::new(false);
+            let message = llm_client.chat("こんにちは").await;
+            println!("{}", message);
+        }
+    }
 }
