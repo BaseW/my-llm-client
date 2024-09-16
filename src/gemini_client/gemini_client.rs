5a1f536ac9eee5b7d283b797f3199cfc9f8ec9c3
wada <wadayugo@keio.jp>
2024-09-16 14:14:00 +0900

    feat: add structs

--- src/gemini_client/gemini_client.rs
+++ src/gemini_client/gemini_client.rs
@@ -1,17 +1,90 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
+#[derive(Deserialize)]
 pub enum GeminiContentRole {
     User,
     Model,
 }
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize)]
 pub struct GeminiContentPart {
     text: String,
 }
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize)]
 pub struct GeminiContent {
     parts: Vec<GeminiContentPart>,
     role: GeminiContentRole,
+}
+
+#[derive(Deserialize)]
+pub enum FinishReason {
+    FinishReasonUnspecified,
+    Stop,
+    MaxTokens,
+    Safety,
+    Recitation,
+    Language,
+    Other,
+    BlockList,
+    ProhibitedContent,
+    SPII,
+    MalformedFunctionCall,
+}
+
+#[derive(Deserialize)]
+pub struct GeminiSafetyRating {}
+
+#[derive(Deserialize)]
+pub struct AttributionSourceId {}
+
+#[derive(Deserialize)]
+pub struct GroundingAttribution {
+    souurceId: AttributionSourceId,
+    content: GeminiContent,
+}
+
+#[derive(Deserialize)]
+pub struct CitationMetadata {}
+
+#[derive(Deserialize)]
+pub struct GeminiContentCandidate {
+    content: GeminiContent,
+    finish_reason: FinishReason,
+    safety_ratings: Vec<GeminiSafetyRating>,
+    citation_metadata: CitationMetadata,
+    token_count: i32,
+    index: usize,
+}
+
+#[derive(Deserialize)]
+pub enum GeminiPromptFeedbackBlockReason {
+    BlockReasonUnspecified,
+    Safety,
+    Other,
+    BlockList,
+    ProhibitedContent,
+}
+
+#[derive(Deserialize)]
+pub struct GeminiPromptFeedbackSafetyRating {}
+
+#[derive(Deserialize)]
+pub struct GeminiPromptFeedback {
+    block_reason: GeminiPromptFeedbackBlockReason,
+    safety_rating: GeminiPromptFeedbackSafetyRating,
+}
+
+#[derive(Deserialize)]
+pub struct GeminiUsageMetadata {
+    prompt_token_count: i32,
+    cached_content_token_count: i32,
+    candidates_token_count: i32,
+    total_token_count: i32,
+}
+
+#[derive(Deserialize)]
+pub struct GeminiContentResponse {
+    prompt_feedback: GeminiPromptFeedback,
+    usage_metadata: GeminiUsageMetadata,
 }
